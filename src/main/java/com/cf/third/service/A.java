ed7fbd393a275186b656013f1fa25da98f50e357
chengfan <1476153957@qq.com>
2021-04-22 20:33:24 +0800

    third message

---
+++ src/main/java/com/cf/third/service/A.java
@@ -0,0 +1,15 @@
+package com.cf.third.service;
+
+
+/**
+ * @author chengfan
+ * @date 2021-04-22 19:58:00
+ */
+public class A {
+
+
+    public static void main(String[] args) {
+        System.out.println("我是third");
+    }
+
+}
